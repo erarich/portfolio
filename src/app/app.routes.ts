50f4b1f3f96a89f5e68c622dcc1596b58e5eca66
Érick Santos <ericksantos@mailfence.com>
2024-01-01 17:59:47 -0300

    feat: adds project guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,22 @@ import { HomeComponent } from './home/home.component';
 import { AreaComponent } from './area/area.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { areaGuard } from './area.guard';
+import { projectGuard } from './project.guard';
+import { ProjectComponent } from './project/project.component';
 
 export const routes: Routes = [
   {
     path: 'area/:type',
     component: AreaComponent,
     canActivate: [areaGuard],
+    data: {
+      router: Router,
+    },
+  },
+  {
+    path: 'project/:id',
+    component: ProjectComponent,
+    canActivate: [projectGuard],
     data: {
       router: Router,
     },
