8b6751b1522f44b307ab119d78fe04375ad26e03
Érick Santos <ericksantos@mailfence.com>
2024-01-09 19:04:28 -0300

    removes standalone: true and imports in components

--- src/app/area-grid/area-grid.component.ts
+++ src/app/area-grid/area-grid.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-area-grid',
-  standalone: true,
-  imports: [],
   templateUrl: './area-grid.component.html',
-  styleUrl: './area-grid.component.css'
+  styleUrl: './area-grid.component.css',
 })
-export class AreaGridComponent {
-
-}
+export class AreaGridComponent {}
--- src/app/area-list/area-list.component.ts
+++ src/app/area-list/area-list.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-area-list',
-  standalone: true,
-  imports: [],
   templateUrl: './area-list.component.html',
-  styleUrl: './area-list.component.css'
+  styleUrl: './area-list.component.css',
 })
-export class AreaListComponent {
-
-}
+export class AreaListComponent {}
