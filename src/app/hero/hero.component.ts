dbd3a78870b209c5d9bdacd46978c585cb695d84
Érick Santos <ericksantos@mailfence.com>
2024-01-09 21:00:57 -0300

    styles hero section

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.css',
 })
 export class HeroComponent {
+  redirectToGitHub(githubLink: string): void {
+    window.location.href = githubLink;
+  }
 
+  redirectToDemo(demoLink: string): void {
+    window.location.href = demoLink;
+  }
 }
