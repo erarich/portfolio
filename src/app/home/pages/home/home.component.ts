ee5308e6e4be0cf48784dfa38e68908cd0ec2072
Érick Santos <ericksantos@mailfence.com>
2024-01-12 20:44:49 -0300

    imports future-features component in home page

--- src/app/home/pages/home/home.component.ts
+++ src/app/home/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 import { HeroComponent } from './hero/hero.component';
 import { AboutComponent } from './about/about.component';
+import { FutureFeaturesComponent } from './future-features/future-features.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, AboutComponent],
+  imports: [HeroComponent, AboutComponent, FutureFeaturesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
